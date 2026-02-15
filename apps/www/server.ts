b5906621e0915e9f02a0bd3836db1f169087bc6c
Benjamin Legrand <benjilegnard@protonmail.com>
2026-02-15 14:07:56 +0100

    refacto: migrate from express to fastify

--- apps/www/server.ts
+++ apps/www/server.ts
@@ -1,70 +1,51 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr/node';
-import express from 'express';
-import { existsSync } from 'node:fs';
-import { join } from 'node:path';
-import bootstrap from './src/main.server';
+import {
+  AngularNodeAppEngine,
+  createNodeRequestHandler,
+  isMainModule,
+  writeResponseToNodeResponse,
+} from '@angular/ssr/node';
+import fastifyStatic from '@fastify/static';
+import fastify from 'fastify';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-// The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
-  const server = express();
-  const distFolder = join(process.cwd(), 'dist/apps/www');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
-    ? join(distFolder, 'index.original.html')
-    : join(distFolder, 'index.html');
+export function app() {
+  const server = fastify();
+  const angularNodeAppEngine = new AngularNodeAppEngine();
+  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+  const browserDistFolder = resolve(serverDistFolder, '../browser');
 
-  const commonEngine = new CommonEngine();
-
-  server.set('view engine', 'html');
-  server.set('views', distFolder);
-
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get(
-    '*.*',
-    express.static(distFolder, {
-      maxAge: '1y',
-    }),
-  );
+  server.register(fastifyStatic, { root: browserDistFolder, wildcard: false });
 
   // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: distFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+  server.get('*', async (req, reply) => {
+    try {
+      const response = await angularNodeAppEngine.handle(req.raw, {
+        server: 'fastify',
+      });
+      if (response) {
+        await writeResponseToNodeResponse(response, reply.raw);
+      } else {
+        reply.callNotFound();
+      }
+    } catch (error) {
+      reply.send(error);
+    }
   });
 
   return server;
 }
 
-function run(): void {
-  const port = process.env['PORT'] || 4000;
+const server = app();
 
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+if (isMainModule(import.meta.url)) {
+  const port = +(process.env['PORT'] || 4000);
+  server.listen({ port }, () => {
+    console.log(`Fastify server listening on http://localhost:${port}`);
   });
 }
 
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
-declare const __non_webpack_require__: NodeRequire;
-const mainModule = __non_webpack_require__.main;
-const moduleFilename = (mainModule && mainModule.filename) || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-  run();
-}
-
-export default bootstrap;
+export const reqHandler = createNodeRequestHandler(async (req, res) => {
+  await server.ready();
+  server.server.emit('request', req, res);
+});
