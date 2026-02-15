6cffb34332393b4b33ec85bb703b863960c188f7
Benjamin Legrand <benjilegnard@protonmail.com>
2026-02-15 15:20:31 +0100

    test: fix tests warnings

--- apps/www/src/test-setup.ts
+++ apps/www/src/test-setup.ts
@@ -1,10 +1,3 @@
-// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
-globalThis.ngJest = {
-  testEnvironmentOptions: {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true,
-  },
-};
 import { setupZonelessTestEnv } from 'jest-preset-angular/setup-env/zoneless';
 
 setupZonelessTestEnv();
